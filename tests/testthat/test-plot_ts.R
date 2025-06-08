4fd347e5de103ddf792a707318a67dc35fe7f75d
andybeet <22455149+andybeet@users.noreply.github.com>
2025-06-08 18:56:13 -0400

    fleshed out plot for plot_ts

--- tests/testthat/test-plot_ts.R
+++ tests/testthat/test-plot_ts.R
@@ -18,4 +18,45 @@ test_that("plot_ts with valid itis and metric", {
   expect_type(p, "list")
   expect_type(p$data, "list")
   expect_s3_class(p$plot, "gg")
+  #abundance
+  p <- plot_ts(itis = 172419, metric = "Abundance",
+               stock="Albacore - North Atlantic")
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
+  # F
+  p <- plot_ts(itis = 172419, metric = "Fmort",
+               stock="Albacore - North Atlantic")
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
+  # Recruitment
+  p <- plot_ts(itis = 172419, metric = "Recruitment",
+               stock="Albacore - North Atlantic")
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
+
+})
+
+# test facetplot
+test_that("plot_ts with facetplot", {
+  p <- plot_ts(itis = 172419, metric = "Catch", facetplot = TRUE)
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
+  # Fmort facet
+  p <- plot_ts(itis = 172419, metric = "Fmort",
+               stock="Albacore - North Atlantic",
+               facetplot = TRUE)
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
+  # Recruitment facet
+  p <- plot_ts(itis = 172419, metric = "Recruitment",
+               stock="Albacore - North Atlantic",
+               facetplot = TRUE)
+  expect_type(p, "list")
+  expect_type(p$data, "list")
+  expect_s3_class(p$plot, "gg")
 })
