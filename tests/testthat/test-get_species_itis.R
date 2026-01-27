d1f3cd36d4913b9656f840bdd18fccc475d7df22
andybeet <22455149+andybeet@users.noreply.github.com>
2026-01-26 22:27:53 -0500

    add test that for changes in get_species_itis to allow for stock

--- tests/testthat/test-get_species_itis.R
+++ tests/testthat/test-get_species_itis.R
@@ -9,16 +9,40 @@ test_that("get_species_itis with null itis", {
   expect_true("ITIS" %in% colnames(res))
   expect_true("StockName" %in% colnames(res))
   expect_true("Jurisdiction" %in% colnames(res))
+  expect_true("StockID" %in% colnames(res))
 })
+
+# test stock id
+test_that("get_species_itis with character stock id", {
+  res <- get_species_itis(stock = "10950")
+  expect_true(nrow(res) > 0)
+  expect_true(any(res$ITIS == 172419))
+  expect_true("ITIS" %in% colnames(res))
+  expect_true("StockName" %in% colnames(res))
+  expect_true("Jurisdiction" %in% colnames(res))
+  expect_true("StockID" %in% colnames(res))
+})
+
+test_that("get_species_itis with numeric stock id", {
+  res <- get_species_itis(stock = 10950)
+  expect_true(nrow(res) > 0)
+  expect_true(any(res$ITIS == 172419))
+  expect_true("ITIS" %in% colnames(res))
+  expect_true("StockName" %in% colnames(res))
+  expect_true("Jurisdiction" %in% colnames(res))
+  expect_true("StockID" %in% colnames(res))
+})
+
 
 # test null stock
 test_that("get_species_itis with specific itis", {
   res <- get_species_itis(itis = 172419)
   expect_true(nrow(res) > 0)
-  expect_true(all(grepl("Albacore",res$StockName)))
+  expect_true(all(grepl("Albacore", res$StockName)))
   expect_true("ITIS" %in% colnames(res))
   expect_true("StockName" %in% colnames(res))
   expect_true("Jurisdiction" %in% colnames(res))
+  expect_true("StockID" %in% colnames(res))
 })
 
 # test null both argument
