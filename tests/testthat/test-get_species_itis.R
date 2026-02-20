f9e7584ee17f7e16b7178b11d790df0a073828ea
andybeet <22455149+andybeet@users.noreply.github.com>
2026-02-20 10:50:23 -0500

    refactor(unit tests): reworked to use updated data set field names

--- tests/testthat/test-get_species_itis.R
+++ tests/testthat/test-get_species_itis.R
@@ -5,32 +5,32 @@
 test_that("get_species_itis with null itis", {
   res <- get_species_itis(stock = "Albacore")
   expect_true(nrow(res) > 0)
-  expect_true(any(res$ITIS == 172419))
-  expect_true("ITIS" %in% colnames(res))
-  expect_true("StockName" %in% colnames(res))
-  expect_true("Jurisdiction" %in% colnames(res))
-  expect_true("StockID" %in% colnames(res))
+  expect_true(any(res$itis == 172419))
+  expect_true("itis" %in% colnames(res))
+  expect_true("stock_name" %in% colnames(res))
+  expect_true("jurisdiction" %in% colnames(res))
+  expect_true("stock_id" %in% colnames(res))
 })
 
 # test stock id
 test_that("get_species_itis with character stock id", {
   res <- get_species_itis(stock = "10950")
   expect_true(nrow(res) > 0)
-  expect_true(any(res$ITIS == 172419))
-  expect_true("ITIS" %in% colnames(res))
-  expect_true("StockName" %in% colnames(res))
-  expect_true("Jurisdiction" %in% colnames(res))
-  expect_true("StockID" %in% colnames(res))
+  expect_true(any(res$itis == 172419))
+  expect_true("itis" %in% colnames(res))
+  expect_true("stock_name" %in% colnames(res))
+  expect_true("jurisdiction" %in% colnames(res))
+  expect_true("stock_id" %in% colnames(res))
 })
 
 test_that("get_species_itis with numeric stock id", {
   res <- get_species_itis(stock = 10950)
   expect_true(nrow(res) > 0)
-  expect_true(any(res$ITIS == 172419))
-  expect_true("ITIS" %in% colnames(res))
-  expect_true("StockName" %in% colnames(res))
-  expect_true("Jurisdiction" %in% colnames(res))
-  expect_true("StockID" %in% colnames(res))
+  expect_true(any(res$itis == 172419))
+  expect_true("itis" %in% colnames(res))
+  expect_true("stock_name" %in% colnames(res))
+  expect_true("jurisdiction" %in% colnames(res))
+  expect_true("stock_id" %in% colnames(res))
 })
 
 
@@ -38,11 +38,11 @@ test_that("get_species_itis with numeric stock id", {
 test_that("get_species_itis with specific itis", {
   res <- get_species_itis(itis = 172419)
   expect_true(nrow(res) > 0)
-  expect_true(all(grepl("Albacore", res$StockName)))
-  expect_true("ITIS" %in% colnames(res))
-  expect_true("StockName" %in% colnames(res))
-  expect_true("Jurisdiction" %in% colnames(res))
-  expect_true("StockID" %in% colnames(res))
+  expect_true(all(grepl("Albacore", res$stock_name)))
+  expect_true("itis" %in% colnames(res))
+  expect_true("stock_name" %in% colnames(res))
+  expect_true("jurisdiction" %in% colnames(res))
+  expect_true("stock_id" %in% colnames(res))
 })
 
 # test null both argument
