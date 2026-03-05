fc35eb9b01a5e19c0f6984ded0950ba0a4428d9d
andybeet <22455149+andybeet@users.noreply.github.com>
2026-03-05 15:56:35 -0500

    style(air) additional formatting of files using air cli

--- data-raw/compareDataLocal.r
+++ data-raw/compareDataLocal.r
@@ -3,14 +3,23 @@
 
 library(magrittr)
 compareDataLocal <- function() {
-
   current <- stocksmart::stockAssessmentSummary
   new <- testapi::stockAssessmentSummary
 
   c1 <- current %>%
-    dplyr::select(`Stock Name`,`ITIS Taxon Serial Number`,`Stock Name`,`Assessment Year`)
+    dplyr::select(
+      `Stock Name`,
+      `ITIS Taxon Serial Number`,
+      `Stock Name`,
+      `Assessment Year`
+    )
   n1 <- new %>%
-    dplyr::select(`Stock Name`,`ITIS Taxon Serial Number`,`Stock Name`,`Assessment Year`)
+    dplyr::select(
+      `Stock Name`,
+      `ITIS Taxon Serial Number`,
+      `Stock Name`,
+      `Assessment Year`
+    )
 
   #current <- testapi::stockAssessmentSummary
   #new <- stocksmart::stockAssessmentSummary
@@ -23,20 +32,19 @@ compareDataLocal <- function() {
   #######################################################
   #######################################################
 
-  if (!all(dim(current) == dim(new))) {# dimensions not same
+  if (!all(dim(current) == dim(new))) {
+    # dimensions not same
     # find if columns added or removed
-      sumcolsAdded <- setdiff(names(new),names(current))
-      sumcolsRemoved <- setdiff(names(current),names(new))
-      sumspeciesAdded <- dplyr::setdiff(n1,c1)
-      sumspeciesRemoved <- dplyr::setdiff(c1,n1)
-
+    sumcolsAdded <- setdiff(names(new), names(current))
+    sumcolsRemoved <- setdiff(names(current), names(new))
+    sumspeciesAdded <- dplyr::setdiff(n1, c1)
+    sumspeciesRemoved <- dplyr::setdiff(c1, n1)
   } else {
     sumcolsAdded <- NULL
     sumcolsRemoved <- NULL
     sumspeciesAdded <- NULL
     sumspeciesRemoved <- NULL
   }
-
 
   #######################################################
   #######################################################
@@ -49,21 +57,33 @@ compareDataLocal <- function() {
 
   #current <- current[1:(nrow(current)-55000),]
 
-  if (!all(dim(current) == dim(new))) { #dimensions not same
+  if (!all(dim(current) == dim(new))) {
+    #dimensions not same
     # find if columns added or removed
-    datcolsAdded <- setdiff(names(new),names(current))
-    datcolsRemoved <- setdiff(names(current),names(new))
+    datcolsAdded <- setdiff(names(new), names(current))
+    datcolsRemoved <- setdiff(names(current), names(new))
 
     newsp <- new %>%
-      dplyr::select(StockName,ITIS,StockArea,Assessmentid,AssessmentYear) %>%
+      dplyr::select(
+        StockName,
+        ITIS,
+        StockArea,
+        Assessmentid,
+        AssessmentYear
+      ) %>%
       dplyr::distinct()
     currentsp <- current %>%
-      dplyr::select(StockName,ITIS,StockArea,Assessmentid,AssessmentYear) %>%
+      dplyr::select(
+        StockName,
+        ITIS,
+        StockArea,
+        Assessmentid,
+        AssessmentYear
+      ) %>%
       dplyr::distinct()
 
-    datspeciesAdded <- dplyr::setdiff(newsp,currentsp)
-    datspeciesRemoved <- dplyr::setdiff(currentsp,newsp)
-
+    datspeciesAdded <- dplyr::setdiff(newsp, currentsp)
+    datspeciesRemoved <- dplyr::setdiff(currentsp, newsp)
   } else {
     datcolsAdded <- NULL
     datcolsRemoved <- NULL
@@ -71,17 +91,17 @@ compareDataLocal <- function() {
     datspeciesRemoved <- NULL
   }
 
-  rmarkdown::render(here::here("data-raw/sendAsEmail.Rmd"),
-                    params = list(
-                      sumrowAdd = sumspeciesAdded,
-                      sumrowRem = sumspeciesRemoved,
-                      sumcolAdd = sumcolsAdded,
-                      sumcolRem = sumcolsRemoved,
-                      datrowAdd = datspeciesAdded,
-                      datrowRem = datspeciesRemoved,
-                      datcolAdd = datcolsAdded,
-                      datcolRem = datcolsRemoved))
-
-
-
+  rmarkdown::render(
+    here::here("data-raw/sendAsEmail.Rmd"),
+    params = list(
+      sumrowAdd = sumspeciesAdded,
+      sumrowRem = sumspeciesRemoved,
+      sumcolAdd = sumcolsAdded,
+      sumcolRem = sumcolsRemoved,
+      datrowAdd = datspeciesAdded,
+      datrowRem = datspeciesRemoved,
+      datcolAdd = datcolsAdded,
+      datcolRem = datcolsRemoved
+    )
+  )
 }
